fdd1e65c59799a8c8b8481a09d34d2a749d254c5
“JonsCodes” <johnalyashai@me.com>
2023-09-26 20:20:34 -0700

    Edited fact.py with facts/models route

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,18 +1,49 @@
-from flask import Blueprint, render_template, request, redirect, url_for 
+from flask import Blueprint, render_template, request, redirect, url_for
+from . import models
 
-# Define a Blueprint for the facts routes
+
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
-# Define the route for submitting new facts
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts')
+
+    results = models.Fact.query.all()
+
+    return render_template('facts/index.html', facts=results)
+
+
 @bp.route('/new')
 def new():
     return render_template('facts/new.html')
 
-# Define the route for listing facts
-@bp.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        print(request.form)
-        return redirect('/facts')
-    
-    return render_template('facts/index.html')
+
+# bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+# @bp.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         submitter = request.form['submitter']
+#         fact_text = request.form['fact']
+
+#         new_fact = models.Fact(submitter=submitter, fact=fact_text)
+
+#         models.db.session.add(new_fact)
+#         models.db.session.commit()
+
+#         return redirect(url_for('fact.index'))
+
+#     results = models.Fact.query.all()
+
+
+# @bp.route('/new')
+# def new():
+#     return render_template('facts/index.html')
